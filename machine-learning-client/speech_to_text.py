aa1dd2e563eea6b357d2e011f4f24efc91c1fe28
sal2948 <sal9791@nyu.edu>
2025-04-11 10:49:39 -0400

    pylint fix

--- machine-learning-client/speech_to_text.py
+++ machine-learning-client/speech_to_text.py
@@ -1,19 +1,9 @@
-"""
-A machine learning client that transforms user speech input
-to text output that will be displayed on the web app, as well as
-text input to speech output.
-
-Uses Google Cloud Speech to Text (through Speech Recognition) and
-Google Cloud Text-to-Speech.
-"""
-
 # ignoring f-string pylint error, as line 36 cannot be an f-string
 # pylint: disable=consider-using-f-string
 
 # assuming we get a .wav or.flac or other audio file as output of getUserMedia()
 import os
 import speech_recognition as sr  # pylint: disable=import-error
-from google.cloud import texttospeech  # pylint: disable=import-error
 from pymongo import MongoClient  # pylint: disable=import-error
 
 mongo_uri = os.environ.get("MONGO_URI")
@@ -45,34 +35,52 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIAL_PATH
 ## need to test
 
 
-class Speech_to_Text:
+class SpeechToText:
+    """
+    A machine learning client that transforms user speech input
+    to text output that will be displayed on the web app, as well as
+    text input to speech output.
+
+    Uses Google Cloud Speech to Text (through Speech Recognition) and
+    Google Cloud Text-to-Speech.
+    """
+
     def initialize(self):
+        """
+        Initializes Google Cloud Speech Recognizer.
+        """
         # create recognizer
         r = sr.Recognizer()
         return r
 
-    def read_user_inp(self, audio_file_name, recognizer):
+    def read_user_inp(self, r):
+        """
+        Reads in user audio as an AudioFile.
+        """
         # using user's un-britishified audio
         audio_file = sentence_collection.find_one({"translated": False})
 
-        if audio_file != None:
+        if audio_file is not None:
             user_inp = sr.AudioFile(str(audio_file["audio"]))
             with user_inp as source:
                 audio = r.record(source)  # records data into AudioData instance
         else:
             print("Could not find audio file. Please try again.")
-            return
+            return None
 
         return audio
 
     def speech_recognition(self, audio, r):
+        """
+        Performs speech recognition and puts user text into database.
+        """
         try:
             print("I think you said: " + r.recognize_google_cloud(audio))
             # r.recognize_google_cloud(audio) is the text output of the audio file
             # Store into original_sentence DB
-            original_text_from_API = r.recognize_google_cloud(audio)
+            original_text_from_api = r.recognize_google_cloud(audio)
             sentence_collection.insert_one(
-                {"original_sentence": original_text_from_API, "britishified": "NONE"}
+                {"original_sentence": original_text_from_api, "britishified": "NONE"}
             )
         except sr.UnknownValueError:
             print("Sorry, could you say that again?")
@@ -85,8 +93,7 @@ class Speech_to_Text:
 
 
 # run
-sp_to_text = Speech_to_Text()
-r = sp_to_text.initialize()
-user_audio_file_str = "machine-learning-client/OSR_us_000_0011_8k.wav"
-user_audio = sp_to_text.read_user_inp(user_audio_file_str, r)
-user_text = sp_to_text.speech_recognition(user_audio, r)
+sp_to_text = SpeechToText()
+recognizer = sp_to_text.initialize()
+user_audio = sp_to_text.read_user_inp(recognizer)
+sp_to_text.speech_recognition(user_audio, recognizer)
--- machine-learning-client/test_speech_to_text.py
+++ machine-learning-client/test_speech_to_text.py
@@ -3,4 +3,4 @@ Unit testing file for ML client:
 currently doing speech-to-text, will test text-to-speech later
 """
 
-import pytest
+# import pytest
