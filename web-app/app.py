3ee3095aa8d867952f3755302f1e284dcafc7812
jasmine7310 <jf4654@nyu.edu>
2025-04-13 11:44:30 -0400

    fix conflit

--- web-app/app.py
+++ web-app/app.py
@@ -16,7 +16,7 @@ mongo_uri = os.environ.get("MONGO_URI")
 mongo_db = os.environ.get("MONGO_DB")
 
 client = MongoClient(mongo_uri)
-db = client["project4_liquidgators"]
+db = client["project4_liquidgators_1"]
 sentence_collection = db["sentences"]
 audio_collection = db["audioFiles"]
 
